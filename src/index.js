26ea3ade68d3332fdc665130a0630cc25fe0e0c6
Tattybubu Tashtanova <tattybubu.tashtanova_2027@ucentralasia.org>
2024-12-29 23:48:28 +0600

    Fix ReactDOM render issue

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(
+// React 18 uses createRoot instead of ReactDOM.render
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
