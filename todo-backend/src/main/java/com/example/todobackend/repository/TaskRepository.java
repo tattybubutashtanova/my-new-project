f1d015a19d17b8b71d24754fff7a21af6df2f04a
tattybubutashtanova <tattybubu.tashtanova_2027@ucentralasia.org>
2024-12-31 03:15:01 +0600

    Delete todo-backend directory

--- todo-backend/src/main/java/com/example/todobackend/repository/TaskRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.todobackend.repository;
-
-import com.example.todobackend.entity.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {}
